a987dc31b6ce847885e2f26f0de2618e010a52e2
Steven Zhu <zhuxinqimac@outlook.com>
2017-10-05 01:56:06 -0500

    Update CIFAR_10_keras_medium.py

--- CIFAR_10_keras_medium.py
+++ CIFAR_10_keras_medium.py
@@ -6,7 +6,6 @@ import keras
 import numpy as np
 import os
 from keras.datasets import cifar10
-from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D, Input
